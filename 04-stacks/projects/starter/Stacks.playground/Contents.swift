972a5318422c4e7696d07c70507c98f2aa8539f9
Trung Nguyen <tmnguyen@epic.com>
2022-04-15 09:19:53 -0500

    Create Stack from Array

--- 04-stacks/projects/starter/Stacks.playground/Contents.swift
+++ 04-stacks/projects/starter/Stacks.playground/Contents.swift
@@ -1,3 +1,50 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+
+public struct Stack<Element> {
+    private var storage: [Element] = []
+    public init() {}
+    
+    public mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    
+    @discardableResult
+    public mutating func pop() -> Element? {
+        storage.popLast()
+    }
+    
+    // Non-essential operations
+    public func peek() -> Element? {
+        storage.last
+    }
+    
+    public var isEmpty: Bool {
+        peek() == nil
+    }
+}
+
+extension Stack: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        """
+        ----top----
+        \(storage.map { "\($0)" }.reversed().joined(separator:
+                                                        "\n"))
+        -----------
+        """
+    }
+}
+
+example(of: "using a stack") {
+    var stack = Stack<Int>()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    print(stack)
+    if let poppedElement = stack.pop() {
+        assert(4 == poppedElement)
+        print("Popped: \(poppedElement)")
+    }
+}
