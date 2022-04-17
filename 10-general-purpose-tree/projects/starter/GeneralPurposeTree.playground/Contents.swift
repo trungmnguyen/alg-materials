40be9892e27d5f2a30c459b10d876ed7b00f0035
Trung Nguyen <tmnguyen@epic.com>
2022-04-17 10:34:04 -0500

    Tree - General Structure

--- 10-general-purpose-tree/projects/starter/GeneralPurposeTree.playground/Contents.swift
+++ 10-general-purpose-tree/projects/starter/GeneralPurposeTree.playground/Contents.swift
@@ -1,3 +1,110 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+public class TreeNode<T> {
+    public var value: T
+    public var children: [TreeNode] = []
+    
+    public init(_ value: T) {
+        self.value = value
+    }
+    
+    public func add(_ child: TreeNode) {
+        children.append(child)
+    }
+}
+
+
+example(of: "creating a tree") {
+    let beverages = TreeNode("Beverages")
+    let hot = TreeNode("Hot")
+    let cold = TreeNode("Cold")
+    beverages.add(hot)
+    beverages.add(cold)
+}
+
+
+extension TreeNode {
+    public func forEachDepthFirst(visit: (TreeNode) -> Void) {
+        visit(self)
+        children.forEach {
+            $0.forEachDepthFirst(visit: visit)
+        }
+    }
+}
+
+func makeBeverageTree() -> TreeNode<String> {
+    let tree = TreeNode("Beverages")
+    let hot = TreeNode("hot")
+    let cold = TreeNode("cold")
+    let tea = TreeNode("tea")
+    let coffee = TreeNode("coffee")
+    let chocolate = TreeNode("cocoa")
+    let blackTea = TreeNode("black")
+    let greenTea = TreeNode("green")
+    let chaiTea = TreeNode("chai")
+    let soda = TreeNode("soda")
+    let milk = TreeNode("milk")
+    let gingerAle = TreeNode("ginger ale")
+    let bitterLemon = TreeNode("bitter lemon")
+    tree.add(hot)
+    tree.add(cold)
+    hot.add(tea)
+    hot.add(coffee)
+    hot.add(chocolate)
+    cold.add(soda)
+    cold.add(milk)
+    tea.add(blackTea)
+    tea.add(greenTea)
+    tea.add(chaiTea)
+    soda.add(gingerAle)
+    soda.add(bitterLemon)
+    return tree
+}
+
+example(of: "depth-first traversal") {
+    let tree = makeBeverageTree()
+    tree.forEachDepthFirst { print($0.value) }
+}
+
+extension TreeNode {
+    public func forEachLevelOrder(visit: (TreeNode) -> Void) {
+        visit(self)
+        var queue = Queue<TreeNode>()
+        children.forEach { queue.enqueue($0) }
+        while let node = queue.dequeue() {
+            visit(node)
+            node.children.forEach { queue.enqueue($0) }
+        }
+    }
+}
+
+example(of: "level-order traversal") {
+    let tree = makeBeverageTree()
+    tree.forEachLevelOrder { print($0.value) }
+}
+
+extension TreeNode where T: Equatable {
+    public func search(_ value: T) -> TreeNode? {
+        var result: TreeNode?
+        forEachLevelOrder { node in
+            if node.value == value {
+                result = node
+            }
+        }
+        return result
+    }
+}
+
+example(of: "searching for a node") {
+    // tree from the last example
+    let tree = makeBeverageTree()
+    if let searchResult1 = tree.search("ginger ale") {
+        print("Found node: \(searchResult1.value)")
+    }
+    if let searchResult2 = tree.search("WKD Blue") {
+        print(searchResult2.value)
+    } else {
+        print("Couldn't find WKD Blue")
+    }
+}
