e945ee07f8debda96108bafda150b7972b8400bc
Trung Nguyen <tmnguyen@epic.com>
2022-04-17 11:22:02 -0500

    Binary Tree - Construct a binary tree and 3 traversals in/pre/post-order

--- 12-binary-tree/projects/starter/BinaryTrees.playground/Contents.swift
+++ 12-binary-tree/projects/starter/BinaryTrees.playground/Contents.swift
@@ -1,3 +1,90 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+public class BinaryNode<Element> {
+    public var value: Element
+    public var leftChild: BinaryNode?
+    public var rightChild: BinaryNode?
+    
+    public init(value: Element) {
+        self.value = value
+    }
+}
+
+var tree: BinaryNode<Int> = {
+  let zero = BinaryNode(value: 0)
+  let one = BinaryNode(value: 1)
+  let five = BinaryNode(value: 5)
+  let seven = BinaryNode(value: 7)
+  let eight = BinaryNode(value: 8)
+  let nine = BinaryNode(value: 9)
+  seven.leftChild = one
+  one.leftChild = zero
+  one.rightChild = five
+  seven.rightChild = nine
+  nine.leftChild = eight
+  return seven
+}()
+
+
+extension BinaryNode: CustomStringConvertible {
+    
+    public var description: String {
+        diagram(for: self)
+    }
+    
+    private func diagram(for node: BinaryNode?,
+                         _ top: String = "",
+                         _ root: String = "",
+                         _ bottom: String = "") -> String {
+        guard let node = node else {
+            return root + "nil\n"
+        }
+        
+        if node.leftChild == nil && node.rightChild == nil {
+            return root + "\(node.value)\n"
+        }
+    
+    return diagram(for: node.rightChild, top + " ", top + "┌──", top + "│ ")
+        + root + "\(node.value)\n"
+        + diagram(for: node.leftChild, bottom + "| ", bottom + "└──", bottom + " ")
+    }
+}
+
+example(of: "tree diagram") {
+    print(tree)
+}
+
+
+extension BinaryNode {
+    public func traverseInOrder(visit: (Element) -> Void) {
+        leftChild?.traverseInOrder(visit: visit)
+        visit(value)
+        rightChild?.traverseInOrder(visit: visit)
+    }
+    
+    public func traversePreOrder(visit: (Element) -> Void) {
+        visit(value)
+        leftChild?.traversePreOrder(visit: visit)
+        rightChild?.traversePreOrder(visit: visit)
+    }
+    
+    public func traversePostOrder(visit: (Element) -> Void) {
+        leftChild?.traversePostOrder(visit: visit)
+        rightChild?.traversePostOrder(visit: visit)
+        visit(value)
+    }
+}
+
+example(of: "in-order traversal") {
+ tree.traverseInOrder { print($0) }
+}
+
+example(of: "pre-order traversal") {
+ tree.traversePreOrder { print($0) }
+}
+
+example(of: "post-order traversal") {
+ tree.traversePostOrder { print($0) }
+}
+
