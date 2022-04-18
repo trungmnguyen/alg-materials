f64da469a8b2a0dc5666eadbd0ec30bb7e33a6c5
Trung Nguyen <tmnguyen@epic.com>
2022-04-17 22:24:53 -0500

    Binary Search Tree - Construct a Binary Search Tree

--- 14-binary-search-tree/projects/starter/BinarySearchTree.playground/Contents.swift
+++ 14-binary-search-tree/projects/starter/BinarySearchTree.playground/Contents.swift
@@ -1,3 +1,157 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+public struct BinarySearchTree<Element: Comparable> {
+    public private(set) var root: BinaryNode<Element>?
+    
+    public init() {}
+}
+
+extension BinarySearchTree: CustomStringConvertible {
+    public var description: String {
+        guard let root = root else { return "empty tree" }
+        return String(describing: root)
+    }
+}
+
+extension BinarySearchTree {
+    public mutating func insert(_ value: Element) {
+        root = insert(from: root, value: value)
+    }
+    
+    private mutating func insert(from node: BinaryNode<Element>?, value: Element) -> BinaryNode<Element> {
+        guard let node = node else {
+            return BinaryNode(value: value)
+        }
+        
+        if value < node.value {
+            node.leftChild = insert(from: node.leftChild, value: value)
+        } else {
+            node.rightChild = insert(from: node.rightChild, value: value)
+        }
+        
+        return node
+    }
+}
+
+example(of: "building a BST") {
+    var bst = BinarySearchTree<Int>()
+    for i in 0..<5 {
+        bst.insert(i)
+    }
+    print(bst)
+}
+
+var exampleTree: BinarySearchTree<Int> {
+  var bst = BinarySearchTree<Int>()
+  bst.insert(3)
+  bst.insert(1)
+  bst.insert(4)
+  bst.insert(0)
+  bst.insert(2)
+  bst.insert(5)
+  return bst
+}
+
+example(of: "building a BST") {
+ print(exampleTree)
+}
+
+extension BinarySearchTree {
+    //O(n) search
+//    public func contains(_ value: Element) -> Bool {
+//        guard let root = root else {
+//            return false
+//        }
+//
+//        var found = false
+//        root.traverseInOrder{
+//            if $0 == value {
+//                found = true
+//            }
+//        }
+//        return found
+//    }
+    
+    //O(logn)
+    public func contains(_ value: Element) -> Bool {
+        var current = root
+        
+        while let node = current {
+            if node.value == value {
+                return true
+            }
+            
+            if value < node.value {
+                current = node.leftChild
+            } else {
+                current = node.rightChild
+            }
+        }
+        return false
+    }
+    
+}
+
+example(of: "finding a node") {
+  if exampleTree.contains(5) {
+    print("Found 5!")
+  } else {
+    print("Couldn’t find 5")
+  }
+}
+
+
+private extension BinaryNode {
+    
+    var min: BinaryNode {
+        leftChild?.min ?? self
+    }
+}
+
+extension BinarySearchTree {
+    public mutating func remove(_ value: Element) {
+        root = remove(node: root, value: value)
+    }
+    
+    
+    private func remove(node: BinaryNode<Element>?, value: Element) -> BinaryNode<Element>?{
+        guard let node = node else {
+            return nil
+        }
+        
+        if value == node.value {
+            
+            if node.leftChild == nil && node.rightChild == nil {
+                return nil
+            }
+            
+            if node.leftChild == nil {
+                return node.rightChild
+            }
+            
+            if node.rightChild == nil {
+                return node.leftChild
+            }
+            
+            node.value = node.rightChild!.min.value
+            node.rightChild = remove(node: node.rightChild, value: node.value)
+            
+        } else if value < node.value {
+            node.leftChild  = remove(node: node.leftChild, value: value)
+        } else {
+            node.rightChild = remove(node: node.rightChild, value: value)
+        }
+        return node
+        
+    }
+}
+
+example(of: "removing a node") {
+  var tree = exampleTree
+  print("Tree before removal:")
+  print(tree)
+  tree.remove(3)
+  print("Tree after removing root:")
+  print(tree)
+}
