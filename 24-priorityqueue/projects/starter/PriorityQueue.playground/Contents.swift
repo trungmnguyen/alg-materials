a4f13f8b8baa4cb8675be265f64cf171976fd829
Trung Nguyen <tmnguyen@epic.com>
2022-04-30 08:04:05 -0500

    Priority Queue - Construct from Heap

--- 24-priorityqueue/projects/starter/PriorityQueue.playground/Contents.swift
+++ 24-priorityqueue/projects/starter/PriorityQueue.playground/Contents.swift
@@ -1,3 +1,31 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+struct PriorityQueue<Element: Equatable>: Queue {
+    
+    private var heap: Heap<Element>
+    
+    init(sort: @escaping (Element, Element) -> Bool, elements: [Element] = []) {
+        heap = Heap(sort: sort, elements: elements)
+    }
+    
+    var isEmpty: Bool {
+      heap.isEmpty
+    }
+    var peek: Element? {
+      heap.peek()
+    }
+    mutating func enqueue(_ element: Element) -> Bool { // 1
+      heap.insert(element)
+    return true
+    }
+    mutating func dequeue() -> Element? { // 2
+      heap.remove()
+    }
+}
+
+var priorityQueue = PriorityQueue(sort: >, elements:
+[1,12,3,4,1,6,8,7])
+while !priorityQueue.isEmpty {
+  print(priorityQueue.dequeue()!)
+}
