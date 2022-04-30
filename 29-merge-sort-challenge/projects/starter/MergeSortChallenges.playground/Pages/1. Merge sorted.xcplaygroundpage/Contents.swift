002cff4aa0ec4847a89618cbffbc57ab7d3fa565
Trung Nguyen <tmnguyen@epic.com>
2022-04-30 13:46:42 -0500

    MergeSort - Construct and Challenges

--- 29-merge-sort-challenge/projects/starter/MergeSortChallenges.playground/Pages/1. Merge sorted.xcplaygroundpage/Contents.swift
+++ 29-merge-sort-challenge/projects/starter/MergeSortChallenges.playground/Pages/1. Merge sorted.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@
 
 let size = 1024
 var values: [Int] = []
-// 1
+values.reserveCapacity(size)
 for i in 0 ..< size {
   values.append(i)
 }
@@ -14,5 +14,47 @@ for i in 0 ..< size {
 
 //: ## Challenge 2: Merge two sequences
 func merge<T: Sequence>(first: T, second: T) -> AnySequence<T.Element> where T.Element: Comparable {
-  fatalError() // placeholder
+    
+    var result: [T.Element] = []
+    
+    var firstIterator = first.makeIterator()
+    var secondIterator = second.makeIterator()
+    
+    
+    var firstNextValue = firstIterator.next()
+    var secondNextValue = secondIterator.next()
+    
+    while let first = firstNextValue,
+          let second = secondNextValue {
+        if first < second {
+            result.append(first)
+            firstNextValue = firstIterator.next()
+        } else if second < first {
+            result.append(second)
+            secondNextValue = secondIterator.next()
+        } else {
+            result.append(first)
+            result.append(second)
+            firstNextValue = firstIterator.next()
+            secondNextValue = secondIterator.next()
+        }
+    }
+    
+    while let first = firstNextValue {
+        result.append(first)
+        firstNextValue = firstIterator.next()
+    }
+    
+    while let second = secondNextValue {
+        result.append(second)
+        secondNextValue = secondIterator.next()
+    }
+    
+    return AnySequence<T.Element>(result)
+}
+
+var array1 = [1, 2, 3, 4, 5, 6, 7, 8]
+var array2 = [1, 3, 4, 5, 5, 6, 7, 7]
+for element in merge(first: array1, second: array2) {
+  print(element)
 }
