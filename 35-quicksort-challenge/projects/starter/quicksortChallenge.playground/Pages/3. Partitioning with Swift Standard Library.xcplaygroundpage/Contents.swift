f3bc6aa2f5d42d155e85f5d791dac66525c2fab4
Trung Nguyen <tmnguyen@epic.com>
2022-05-01 10:14:58 -0500

    Quicksort - Challenges

--- 35-quicksort-challenge/projects/starter/quicksortChallenge.playground/Pages/3. Partitioning with Swift Standard Library.xcplaygroundpage/Contents.swift
+++ 35-quicksort-challenge/projects/starter/quicksortChallenge.playground/Pages/3. Partitioning with Swift Standard Library.xcplaygroundpage/Contents.swift
@@ -11,3 +11,29 @@
  */
 
 //: [Next](@next)
+
+
+extension MutableCollection where Self: BidirectionalCollection, Element: Comparable {
+
+    mutating func quicksort() {
+      quicksortLumuto(low: startIndex, high: index(before: endIndex))
+    }
+    
+    private mutating func quicksortLumuto(low: Index, high: Index) {
+        if low < high {
+            let pivotValue = self[high]
+            var p = self.partition { $0 > pivotValue }
+            
+            if p == endIndex {
+                p = index(before: p)
+            }
+            
+            self[..<p].quicksortLumuto(low: low, high: index(before: p))
+            self[p...].quicksortLumuto(low: index(after: p), high: high)
+        }
+    }
+}
+var numbers = [12, 0, 3, 9, 2, 21, 18, 27, 1, 5, 8, -1, 8]
+print(numbers)
+numbers.quicksort()
+print(numbers)
